11e06fd44a71e44ebee261660e77be956391b383
Alex Jensen <96dalex@gmail.com>
2016-03-02 16:43:06 -0700

    logging now includes time

--- slacktor.rb
+++ slacktor.rb
@@ -1,5 +1,11 @@
 require "slack/client"
 require "json"
+require "date"
+
+def log_puts(arg)
+  time = Date.today.to_s + " " + Time.now.strftime("%I:%M %p")
+  puts "#{time} - #{arg}"
+end
 
 def get_swig(client)
   list = JSON.parse(client.channels.list)
@@ -7,12 +13,12 @@ def get_swig(client)
   if swig
     swig
   else
-    puts "ERROR: channel #dj_swig not found!!! sally probably archived it"
+    log_puts "ERROR: channel #dj_swig not found!!! sally probably archived it"
   end
 end
 
 def run
-  puts "Warming up the Slacktor Beam..."
+  log_puts "Warming up the Slacktor Beam..."
   token = ENV["SLACK_TOKEN"]
   client = Slack::Client.new(token: token)
 
@@ -20,7 +26,7 @@ def run
   channel_id = swig["id"]
   members = swig["members"]
 
-  puts "Slacktor Beam active"
+  log_puts "Slacktor Beam active"
   loop do
     sleep 10
 
@@ -30,10 +36,10 @@ def run
       members_missing = members - new_members
       members = (members + new_members).uniq
       if members_missing != []
-        puts "Users: #{members_missing.inspect} are trying to leave!"
+        log_puts "Users: #{members_missing.inspect} are trying to leave!"
       end
       members_missing.each do |user_id|
-        puts "Re-inviting escapee #{user_id}"
+        log_puts "Re-inviting escapee #{user_id}"
         client.channels.get("channels.invite", channel: channel_id, user: user_id)
       end
     end
