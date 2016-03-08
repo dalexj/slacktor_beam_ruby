efc2cc9d7eb1057ac42a7918744898b78c517f04
Alex Jensen <96dalex@gmail.com>
2016-03-08 09:10:46 -0700

    unarchive support

--- slacktor.rb
+++ slacktor.rb
@@ -13,8 +13,24 @@ def get_swig(client)
   if swig
     swig
   else
-    log_puts "ERROR: channel #dj_swig not found!!! sally probably archived it"
+    log_puts "ERROR: channel #dj_swig not found, must have been admin deleted"
+    log_puts "Re-run slacktor beam once the channel is recreated"
+    exit 0
   end
+end
+
+def reinvite_missing(client, swig, members, archive)
+  new_members = swig["members"]
+  members_missing = members - new_members
+  members = (members + new_members).uniq
+  if members_missing != [] && !archive
+    log_puts "Users: #{members_missing.inspect} are trying to leave!"
+  end
+  members_missing.each do |user_id|
+    log_puts "Re-inviting escapee #{user_id}"
+    client.channels.get("channels.invite", channel: swig["id"], user: user_id)
+  end
+  members
 end
 
 def run
@@ -23,7 +39,6 @@ def run
   client = Slack::Client.new(token: token)
 
   swig = get_swig(client)
-  channel_id = swig["id"]
   members = swig["members"]
 
   log_puts "Slacktor Beam active"
@@ -32,15 +47,14 @@ def run
 
     swig = get_swig(client)
     if swig
-      new_members = swig["members"]
-      members_missing = members - new_members
-      members = (members + new_members).uniq
-      if members_missing != []
-        log_puts "Users: #{members_missing.inspect} are trying to leave!"
-      end
-      members_missing.each do |user_id|
-        log_puts "Re-inviting escapee #{user_id}"
-        client.channels.get("channels.invite", channel: channel_id, user: user_id)
+      if swig["is_archived"]
+        log_puts "The swig has been archived, un-archiving channel..."
+        client.channels.get("channels.unarchive", channel: swig["id"])
+        log_puts "The swig has been unarchived"
+        log_puts "Re-inviting all members..."
+        members = reinvite_missing(client, swig, members, true)
+      else
+        members = reinvite_missing(client, swig, members, false)
       end
     end
   end
