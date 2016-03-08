2d4260c6c4cb3aa8e840ff7b03103c7e4e320a4c
Alex Jensen <96dalex@gmail.com>
2016-03-08 12:15:59 -0700

    refactor to class

--- slacktor.rb
+++ slacktor.rb
@@ -2,63 +2,71 @@ require "slack/client"
 require "json"
 require "date"
 
-def log_puts(arg)
-  time = Date.today.to_s + " " + Time.now.strftime("%I:%M %p")
-  puts "#{time} - #{arg}"
-end
+class SlacktorBeam
+  def initialize
+    log_puts "Warming up the Slacktor Beam..."
 
-def get_swig(client)
-  list = JSON.parse(client.channels.list)
-  swig = list["channels"].find { |chan| chan["name"] == "dj_swig" }
-  if swig
-    swig
-  else
-    log_puts "ERROR: channel #dj_swig not found, must have been admin deleted"
-    log_puts "Re-run slacktor beam once the channel is recreated"
-    exit 0
+    @channel_name = "dj_swig"
+    @client = Slack::Client.new(token: ENV["SLACK_TOKEN"])
+
+    lookup_swig
+    @members = @swig["members"]
+
+    log_puts "Slacktor Beam active"
   end
-end
 
-def reinvite_missing(client, swig, members, archive)
-  new_members = swig["members"]
-  members_missing = members - new_members
-  members = (members + new_members).uniq
-  if members_missing != [] && !archive
-    log_puts "Users: #{members_missing.inspect} are trying to leave!"
+  def log_puts(arg)
+    time = Date.today.to_s + " " + Time.now.strftime("%I:%M %p")
+    puts "#{time} - #{arg}"
   end
-  members_missing.each do |user_id|
-    log_puts "Re-inviting escapee #{user_id}"
-    client.channels.get("channels.invite", channel: swig["id"], user: user_id)
+
+  def lookup_swig
+    list = JSON.parse(@client.channels.list)
+    @swig = list["channels"].find { |chan| chan["name"] == @channel_name }
+    check_for_admin_abuse!
   end
-  members
-end
 
-def run
-  log_puts "Warming up the Slacktor Beam..."
-  token = ENV["SLACK_TOKEN"]
-  client = Slack::Client.new(token: token)
+  def check_for_admin_abuse!
+    if !@swig
+      log_puts "ERROR: channel ##{@channel_name} not found, must have been deleted by an admin"
+      log_puts "Re-run slacktor beam once the channel is recreated"
+      exit 0
+    end
+  end
 
-  swig = get_swig(client)
-  members = swig["members"]
+  def run
+    lookup_swig
+    if @swig["is_archived"]
+      log_puts "The swig has been archived, un-archiving channel..."
+      unarchive_swig
+      log_puts "The swig has been unarchived"
+      log_puts "Re-inviting all members..."
+      reinvite_missing(true)
+    else
+      reinvite_missing(false)
+    end
+  end
 
-  log_puts "Slacktor Beam active"
-  loop do
-    sleep 10
+  def unarchive_swig
+    @client.channels.get("channels.unarchive", channel: @swig["id"])
+  end
 
-    swig = get_swig(client)
-    if swig
-      if swig["is_archived"]
-        log_puts "The swig has been archived, un-archiving channel..."
-        client.channels.get("channels.unarchive", channel: swig["id"])
-        log_puts "The swig has been unarchived"
-        log_puts "Re-inviting all members..."
-        members = reinvite_missing(client, swig, members, true)
-      else
-        members = reinvite_missing(client, swig, members, false)
-      end
+  def reinvite_missing(archive)
+    new_members = @swig["members"]
+    members_missing = @members - new_members
+    @members = (@members + new_members).uniq
+    if members_missing != [] && !archive
+      log_puts "Users: #{members_missing.inspect} are trying to leave!"
+    end
+    members_missing.each do |user_id|
+      log_puts "Re-inviting escapee #{user_id}"
+      @client.channels.get("channels.invite", channel: @swig["id"], user: user_id)
     end
   end
 end
 
-
-run
+beam = SlacktorBeam.new
+loop do
+  sleep 10
+  beam.run
+end
